e2117095f1061ecc1b443c4ccb03e23a8ac97eb2
Wirebird Labs LLC <jack@wirebirdlabs.com>
2015-01-31 20:32:52 -0600

    Installing LVRTE on Linux with alien

--- kernel/.travis.install.sh
+++ kernel/.travis.install.sh
@@ -25,10 +25,14 @@
 # Configure this script to exit on error, skipping downstream commands
 set -e
 
+# Needed to convert Red Hat to Debian package
+sudo apt-get install alien
+
 mkdir download && \
 cd download && \
 wget http://ftp.ni.com/support/softlib/labview/labview_runtime/2014/Linux/LabVIEW2014RTE_Linux64.tgz -O lvrte.tgz && \
 tar -xzf lvrte.tgz && \
-ls
+ls && \
+sudo alien --install --scripts labview-2014-rte-14.0.0-1.x86_64.rpm
 # sudo rpm -Uvh *.rpm
 
