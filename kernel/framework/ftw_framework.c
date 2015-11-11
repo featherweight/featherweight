830f073409e30122335ea907598412740d8784e0
Jack R. Dunaway <jack@wirebirdlabs.com>
2015-11-11 09:09:50 -0600

    Adding `framework` to the ftw-kernel, which combines features of component libraries for performance and convenience for framework-level abilities

---
+++ kernel/framework/ftw_framework.c
@@ -0,0 +1,60 @@
+/*
+    Copyright (c) 2014-2015 Wirebird Labs LLC. All rights reserved.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom
+    the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+#include "ftw_framework.h"
+
+json_t *ftw_framework_router_recv(void *msg, size_t msg_len, size_t flags, int64 *err_line,
+        int64 *err_column, int64 *err_position, LStrHandle err_source, LStrHandle err_hint)
+{
+    json_error_t err;
+    json_t *parsed;
+
+    parsed = json_loadb(msg, msg_len, flags, &err);
+
+    /*  Invalid JSON in the buffer is an error condition indicated by NULL. */
+    if (parsed == NULL) {
+        *err_line = err.line;
+        *err_column = err.column;
+        *err_position = err.position;
+        ftw_support_CStr_to_LStrHandle(&err_source, err.source);
+        ftw_support_CStr_to_LStrHandle(&err_hint, err.text);
+    }
+
+    return parsed;
+}
+
+const char *ftw_version(void)
+{
+    return "0.6.0";
+}
+
+void ftw_lvmem(int64 *ds_bytes_allocated)
+{
+    MgErr lv_err;
+    MemStatRec stats;
+    lv_err = DSMemStats(&stats);
+    if (lv_err)
+        *ds_bytes_allocated = -1;
+    else
+        *ds_bytes_allocated = (int64)stats.totAllocSize;
+    return;
+}
