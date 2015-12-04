a5f0c730906719519a51ae768e41391898261732
Jack R. Dunaway <jack@wirebirdlabs.com>
2015-12-04 15:44:06 -0600

    Travis CI development

--- kernel/.travis.install.sh
+++ kernel/.travis.install.sh
@@ -20,21 +20,18 @@
 #    IN THE SOFTWARE.
 #
 
-# This script configures the Travis CI environment prior to automated build
+# This script downloads and installs the LabVIEW Runtime Engine for Linux
 
 # Configure this script to exit on error, skipping downstream commands
 set -e
 
-# Needed to convert Red Hat to Debian package
-sudo apt-get update
-sudo apt-get install alien
+LVSRC=http://ftp.ni.com/support/softlib/labview/labview_runtime/2014/Linux/LabVIEW2014RTE_Linux64.tgz
+LVPKG=labview-2014-rte-14.0.0-1.x86_64.rpm
 
-mkdir download && \
-cd download && \
-wget http://ftp.ni.com/support/softlib/labview/labview_runtime/2014/Linux/LabVIEW2014RTE_Linux64.tgz -O lvrte.tgz && \
-tar -xzf lvrte.tgz && \
-ls && \
-sudo alien --install --scripts labview-2014-rte-14.0.0-1.x86_64.rpm && \
-ls /usr/local/lib
-# sudo rpm -Uvh *.rpm
-
+if test -e $LVPKG; then
+  echo "Found cached LVRTE package: $LVPKG"
+else
+  echo "Did not find cached package: $LVPKG"
+  wget $LVSRC -O lvrte.tgz && \
+  tar -xzf lvrte.tgz
+fi
