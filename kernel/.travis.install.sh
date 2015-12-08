d4024288f630ffa428bab3cec3f81d0e5013c810
Jack R. Dunaway <jack@wirebirdlabs.com>
2015-12-08 09:49:59 -0600

    Travis CI development

--- kernel/.travis.install.sh
+++ kernel/.travis.install.sh
@@ -27,8 +27,9 @@ set -ve
 
 printenv
 
-LVSRC=http://ftp.ni.com/support/softlib/labview/labview_runtime/2014/Linux/LabVIEW2014RTE_Linux64.tgz
-LVPKG=labview-2014-rte-14.0.0-1.x86_64.rpm
+LV_DOWNLOAD=http://ftp.ni.com/support/softlib/labview/labview_runtime/2014/Linux/LabVIEW2014RTE_Linux64.tgz
+LV_PKG_ORIG=labview-2014-rte-14.0.0-1.x86_64.rpm
+LV_PKG_DEST=labview-2014-rte-14.0.0-1.x86_64.deb
 LVDIR=$TRAVIS_BUILD_DIR/lvrte
 
 mkdir -p $1
@@ -36,23 +37,25 @@ cd $1
 ls -al
 
 # Test for presence of cached packages in download cache
-if test -e $LVPKG; then
-  echo "Found cached LVRTE package: $LVPKG"
+if test -e $LV_PKG_ORIG; then
+  echo "Found cached LVRTE package: $LV_PKG_ORIG"
 else
-  echo "Did not find cached package: $LVPKG"
-  wget $LVSRC -O lvrte.tgz && \
+  echo "Did not find cached package: $LV_PKG_ORIG"
+  wget $LV_DOWNLOAD -O lvrte.tgz && \
   tar -xzf lvrte.tgz
 fi
 
-#fakeroot alien --to-deb --scripts --veryverbose labview-2014-rte-14.0.0-1.x86_64.rpm
+chroot $LVDIR
+
+alien --to-deb --scripts --veryverbose $LV_PKG_ORIG
 #ls -al
 set +e
-sudo alien --install --veryverbose --scripts $LVPKG
+#sudo alien --install --veryverbose --scripts $LVPKG
 
 #mkdir $LVDIR
 #mkdir $LVDIR/dpkg
 
 
-#dpkg --install --force-not-root --root=$LVDIR --log=../dpkg.log --debug=2000 labview-2014-rte-14.0.0-1.x86_64.deb
+dpkg --install --force-not-root --root=$LVDIR --log=../dpkg.log --debug=2000 $LV_PKG_DEST
 
 cat ../dpkg.log
