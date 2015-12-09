3e0a9e261f5a9c6ae9c55273ba5470359352cb25
Jack R. Dunaway <jack@wirebirdlabs.com>
2015-12-09 16:54:39 -0600

    Turning off Autobahn TestSuite until: https://github.com/crossbario/autobahn-testsuite/issues/55

--- kernel/build-ftw.sh
+++ kernel/build-ftw.sh
@@ -29,7 +29,7 @@ set -ve
 ./autogen.sh
 
 # Configure for this platform
-./configure LVRTE_DIR=$2 LVRTE_LIB=$1
+./configure LVRTE_DIR=$2 LVRTE_LIB=$1 --enable-autobahntest=no
 
 # Generate libraries
 make
