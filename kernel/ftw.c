b0eea8e4c379b5ef2041acf799a5081499d5ff11
Jack R. Dunaway <jack@wirebirdlabs.com>
2017-03-28 07:21:13 -0500

    Bump copyright year to 2017

--- kernel/ftw.c
+++ kernel/ftw.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2014-2016 Wirebird Labs LLC. All rights reserved.
+    Copyright (c) 2014-2017 Wirebird Labs LLC. All rights reserved.
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"),
